921425fcaf3b9543a32d151bf76bdb74ab848c77
lordeko <mattia.ruggeri88@gmail.com>
2020-03-17 22:30:04 +0100

    Some improves & comments
    Comments are very important, i know!

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,9 +1,10 @@
-N=int(input('How many Fibonacci numbers do you want generate?'))
+#user input
+N=int(input('How many Fibonacci numbers do you want generate? '))
 F=[1,1]
-i=len(F)
+i=2 #actually len(F)
 if N>2:
     while i<N:
-        nxt=sum(F[i-2:])
-        F.append(nxt)
-        i+=1
+        nxt=sum(F[i-2:])    #calculate next number = sum of the last two elements
+        F.append(nxt)       #append it
+        i+=1                #i=len(F)
 print(F)

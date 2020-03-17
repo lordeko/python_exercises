3c0a9928cbf218b5f711d6b22a33f5e309b58a94
lordeko <mattia.ruggeri88@gmail.com>
2020-03-17 22:58:43 +0100

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+S=input('Write the word to check... \n')
+i=0
+n=len(S)
+s=[]
+while i<n:
+    s.append(S[n-i-1])
+    if s[i]!=S[i]:
+        print(S+' is a palindrome')
+        break
+    i+=1
+if i==n:
+    print(S+' is not a palindrome')
